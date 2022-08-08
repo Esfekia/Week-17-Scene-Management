7584343153fb4c8813c90622b436fbcadb14b78a
Esfekia <sertac@gmail.com>
2022-08-08 13:53:14 -0400

    We utilized the XRRigOrigin tags to ensure that at each scene transition we use the original XR Rig location for our Persistent XR Origin by tweaking SceneMenu and SceneInfo scripts.
    We utilized the XRRigOrigin tags to ensure that at each scene transition we use the original XR Rig location for our Persistent XR Origin by tweaking SceneMenu and SceneInfo scripts.

--- Assets/Scripts/Editor/SceneMenu.cs
+++ Assets/Scripts/Editor/SceneMenu.cs
@@ -12,29 +12,30 @@ public static class SceneMenu
     [MenuItem("Scenes/Lobby")]
     static void OpenLobby()
     {
-        OpenScene(SceneInfo.Names.Lobby);
+        OpenScene(SceneUtils.Names.Lobby);
     }
     [MenuItem("Scenes/Maze")]
     static void OpenMaze()
     {
-        OpenScene(SceneInfo.Names.Maze);
+        OpenScene(SceneUtils.Names.Maze);
     }
     [MenuItem("Scenes/MoodMatic")]
     static void OpenMoodMatic()
     {
-        OpenScene(SceneInfo.Names.MoodMatic);
+        OpenScene(SceneUtils.Names.MoodMatic);
     }
 
     static void OpenScene(string name)
     {
         // make sure this is the active scene when we open up since it has the XR interaction manager
         // single mode closes all open scenes and opens the scene specified:
-        Scene persistentScene = EditorSceneManager.OpenScene("Assets/Scenes/" + SceneInfo.Names.XRPersistent + ".unity", OpenSceneMode.Single);
+        Scene persistentScene = EditorSceneManager.OpenScene("Assets/Scenes/" + SceneUtils.Names.XRPersistent + ".unity", OpenSceneMode.Single);
         
         // Additive: add a scene to the current scene and open it.
         Scene currentScene = EditorSceneManager.OpenScene("Assets/Scenes/" + name + ".unity", OpenSceneMode.Additive);
 
-
+        // Align the positions of Persistent Scene XR Origin with the XR Rig/Origins in other "target" scenes.
+        SceneUtils.AlignXRRig(persistentScene, currentScene);
 
     }
 
---
+++ Assets/Scripts/SceneUtils.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public static class SceneUtils
+{
+    public static class Names
+    {
+        public static readonly string XRPersistent = "XR Persistent";
+        public static readonly string Maze = "Maze";
+        public static readonly string MoodMatic = "MoodMatic";
+        public static readonly string Lobby = "Lobby";
+    }
+
+    // Align the position of our PersistentScene XR Origin with the XR Rigs found in our other scenes.
+    public static void AlignXRRig(Scene persistentScene, Scene currentScene)
+    {
+        GameObject[] currentObjects = currentScene.GetRootGameObjects();
+        GameObject[] persistentObjects = persistentScene.GetRootGameObjects();
+        foreach (var origin in currentObjects)
+        {
+            if (origin.CompareTag("XRRigOrigin"))
+            {
+                foreach(var rig in persistentObjects)
+                { 
+                    if(rig.CompareTag("XRRig"))
+                    {
+                        rig.transform.position = origin.transform.position;
+                        rig.transform.rotation = origin.transform.rotation;
+                        return;
+                    }
+                }
+            }
+        }
+
+    }
+
+}
