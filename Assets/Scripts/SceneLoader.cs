1a493d63ab7e77180badf310aa523a34be940e19
Esfekia <sertac@gmail.com>
2022-08-09 07:46:21 -0400

    Deleted and re-imported the settings for Lobby scene for correct lighting settings.
    Deleted and re-imported the settings for Lobby scene for correct lighting settings.

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -6,12 +6,42 @@ using UnityEngine.Events;
 
 public class SceneLoader : Singleton<SceneLoader>
 {
-    public Material screenfade = null;
+    public Material screenFade = null;
     
+    [Min(0.001f)]
+    public float fadeSpeed = 1.0f;
+    [Range(0.0f, 5.0f)]
+    public float addedWaitTime = 2.0f;
+   
     public UnityEvent onLoadStart = new UnityEvent();
     public UnityEvent onLoadFinish = new UnityEvent();
 
     bool m_isLoading = false;
+
+    // control whether we are at 9 or 1 
+    float m_fadeAmount = 0.0f;
+
+    // keep track of the coroutine that is doing the fading,
+    // in case we need to stop that coroutine and start it for fading in and out quickly
+    Coroutine m_fadeCoroutine = null;
+
+    // cache the shader property ID by using the m_fadeAmount above
+    static readonly int m_fadeAmountPropID = Shader.PropertyToID("_FadeAmount");
+
+    Scene m_persistentScene;
+
+    private void Awake()
+    {
+        SceneManager.sceneLoaded += SetActiveScene;
+
+        //get the active scene when the script wakes up so that we can reference the persistent scene
+        m_persistentScene = SceneManager.GetActiveScene();
+    }
+
+    private void OnDestroy()
+    {
+        SceneManager.sceneLoaded -= SetActiveScene;
+    }
     public void LoadScene(string name)
     {
         if (!m_isLoading)
@@ -22,6 +52,16 @@ public class SceneLoader : Singleton<SceneLoader>
 
     }
 
+    void SetActiveScene(Scene scene, LoadSceneMode mode)
+    {
+        SceneManager.SetActiveScene(scene);
+
+        //Once this scene is loaded and is set active we will use sceneUtils to aling the XR rig/origin
+        SceneUtils.AlignXRRig(m_persistentScene, scene);
+
+    }
+
+
     IEnumerator Load(string name)
     {
         m_isLoading = true;
@@ -30,6 +70,9 @@ public class SceneLoader : Singleton<SceneLoader>
         onLoadStart?.Invoke();
         yield return FadeOut();
         yield return StartCoroutine(UnloadCurrentScene());
+
+        // to ensure the fade in out is not instantaneous
+        yield return new WaitForSeconds(addedWaitTime);
 
         yield return StartCoroutine(LoadCurrentScene(name));
         yield return FadeIn();
@@ -57,13 +100,35 @@ public class SceneLoader : Singleton<SceneLoader>
 
     IEnumerator FadeOut()
     {
-
+        if (m_fadeCoroutine != null)
+        {
+            StopCoroutine(m_fadeCoroutine);
+        }
+        m_fadeCoroutine = StartCoroutine(Fade(1.0f));
+        yield return m_fadeCoroutine;
     }
 
     IEnumerator FadeIn()
     {
-
+        if (m_fadeCoroutine != null)
+        {
+            StopCoroutine(m_fadeCoroutine);
+        }
+        m_fadeCoroutine = StartCoroutine(Fade(0.0f));
+        yield return m_fadeCoroutine;
     }
 
+    IEnumerator Fade(float target)
+    {
+        // update our fade amount and pass it to the shader in the material
+        while (!Mathf.Approximately(m_fadeAmount, target))
+        {
+            m_fadeAmount = Mathf.MoveTowards(m_fadeAmount, target, fadeSpeed * Time.deltaTime);
+            screenFade.SetFloat(m_fadeAmountPropID, m_fadeAmount);
+            yield return null;
+        }
+
+        screenFade.SetFloat(m_fadeAmountPropID, m_fadeAmount);
+    }
 
 }
